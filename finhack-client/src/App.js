31433c83c8ff0e47df7474f3a895820bb630377d
CodeJunkie97 <ianmanners26@outlook.com>
2024-02-15 11:55:37 -0600

    Adding Particles to background
    Added TS Particles configuration to make the front end of the site looks more nice and clean for the user

--- finhack-client/src/App.js
+++ finhack-client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Particle from './components/Particle/Particle';
 
 function App() {
   return (
     <div className="App">
+      <Particle className = "particle"/>
       <Navigation />
       <Logo />
     </div>
